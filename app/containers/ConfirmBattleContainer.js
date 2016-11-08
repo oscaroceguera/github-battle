d9d39135a6d95541e5a3d328585779fe9c31ed99
oscar <oscaroceguera@gmail.com>
2016-11-07 17:04:11 -0700

    React lifecycle events and conditional redering

---
+++ app/containers/ConfirmBattleContainer.js
@@ -0,0 +1,38 @@
+var React = require('react')
+var ConfirmBattle = require('../components/ConfirmBattle')
+
+var ConfirmBattleContainer = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
+  getInitialState: function () {
+    console.log('getInitialState');
+    return {
+      isLoading: true,
+      playerInfo: []
+    }
+  },
+  componentWillMount: function () {
+    console.log('componentWillMount');
+  },
+  componentDidMount: function () {
+    console.log('componentDidMount');
+    var query = this.props.location.query
+    // Fetch info from github then update state
+  },
+  componentWillReciveProps: function () {
+    console.log('componentWillReciveProps');
+  },
+  componentWillUnmount: function () {
+    console.log('componentWillUnmount');
+  },
+  render: function () {
+    return (
+      <ConfirmBattle
+      isLoading={this.state.isLoading}
+      playersInfo={this.state.playerInfo} />
+    )
+  }
+})
+
+module.exports = ConfirmBattleContainer
