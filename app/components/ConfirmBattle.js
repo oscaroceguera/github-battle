5e2347f88c7996d483e954ddbebacdeddbca7e5e
oscar <oscaroceguera@gmail.com>
2016-11-09 01:41:47 -0700

    Private Stateless Functional React Components

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,18 +1,15 @@
 var React = require('react')
 var PropTypes = React.PropTypes
-var styles= require('../styles')
+var styles = require('../styles')
 var Link = require('react-router').Link
 var UserDetails = require('./UserDetails')
 var UserDetailsWrapper = require('./UserDetailsWrapper.js')
-
-function puke (object) {
-  return <pre>{JSON.stringify(object, null, '')}</pre>
-}
+var MainContainer = require('./MainContainer')
 
 function ConfirmBattle (props) {
   return props.isLoading === true
     ? <p> LOADING </p>
-    : <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+    : <MainContainer>
         <h1>Confirm Players</h1>
         <div className='col-sm-8 col-sm-offset-2'>
           <UserDetailsWrapper header='Palyer One'>
@@ -32,7 +29,7 @@ function ConfirmBattle (props) {
             </Link>
           </div>
         </div>
-      </div>
+      </MainContainer>
 }
 
 ConfirmBattle.propTypes = {
